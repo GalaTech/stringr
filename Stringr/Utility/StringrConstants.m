8feff51645c8e8106c1fd26a962a8242d8d715fb
NewTube <online@newtubeapp.com>
2013-11-27 12:00:07 -0700

    Forgot to add a couple of external controls to the previous commit.

---
+++ Stringr/Utility/StringrConstants.m
@@ -0,0 +1,62 @@
+//
+//  StringrConstants.m
+//  Stringr
+//
+//  Created by Jonathan Howard on 11/18/13.
+//  Copyright (c) 2013 GalaTech LLC. All rights reserved.
+//
+
+
+#pragma mark - PFObject Activity Class
+// Class Key
+extern NSString *const kStringrActivityClassKey;
+
+// Field Keys
+extern NSString *const kStringrActivityTypeKey;
+extern NSString *const kStringrFromUserKey;
+extern NSString *const kStringrToUserKey;
+extern NSString *const kStringrContentKey;
+extern NSString *const kStringrPhotoKey;
+
+
+// Type values
+extern NSString *const kStringrActivityTypeStringLike;
+extern NSString *const kStringrActivityTypeStringComment;
+
+extern NSString *const kStringrActivityTypePhotoLike;
+extern NSString *const kStringrActivityTypePhotoComment;
+
+extern NSString *const kStringrActivityTypeFollow;
+extern NSString *const kStringrActivityTypeJoin;
+
+
+#pragma mark - PFObject User Class
+// Field Keys
+extern NSString *const kStringrUserDisplayNameKey;
+extern NSString *const kStringrFacebookIDKey;
+
+extern NSString *const kStringrUserProfilePicThumbnailKey;
+extern NSString *const kStringrUserProfilePicKey;
+
+extern NSString *const kStringrUserAboutDescriptionKey;
+extern NSString *const kStringrUserUniversityKey;
+
+extern NSString *const kStringrUserPrivateChannelKey;
+
+
+
+
+#pragma mark - PFObject StringrString class
+// Class Key
+extern NSString *const kStringrStringClassKey;
+
+// Field Keys
+
+
+
+
+#pragma mark - PFObject Photo Class
+// Class Key
+extern NSString *const kStringrPhotoClassKey;
+
+// Field Keys
