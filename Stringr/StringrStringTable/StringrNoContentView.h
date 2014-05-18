ec6b56baf09fc7bc409d1630acd540fef1e512d0
GalaTech <online@galatech.co>
2014-05-18 17:16:30 -0600

    Redid delegation for multiple classes. Implemented delegation for comment table view and write comment VC so that callbacks are present upon the completion of posting or removing a comment. Documented code throughout the application. Cleaned up implementation of several classes.

---
+++ Stringr/StringrStringTable/StringrNoContentView.h
@@ -0,0 +1,13 @@
+//
+//  StringrNoContentView.h
+//  Stringr
+//
+//  Created by Jonathan Howard on 5/18/14.
+//  Copyright (c) 2014 GalaTech LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StringrNoContentView : UIView
+
+@end
---
+++ Stringr/StringrStringTable/StringrNoContentView.m
@@ -0,0 +1,31 @@
+//
+//  StringrNoContentView.m
+//  Stringr
+//
+//  Created by Jonathan Howard on 5/18/14.
+//  Copyright (c) 2014 GalaTech LLC. All rights reserved.
+//
+
+#import "StringrNoContentView.h"
+
+@implementation StringrNoContentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
