8e2b63659253d6dd7df56e0ad1d37ce8e91ec2ab
Jonathan Howard <JHoward@DoubleEncore.com>
2014-11-04 20:58:27 -0700

    Added some properties to string object.

--- Stringr/Stringr Objects/StringrString.h
+++ Stringr/Stringr Objects/StringrString.h
@@ -10,4 +10,16 @@
 
 @interface StringrString : StringrObject
 
+@property (strong, nonatomic) PFObject *string;
+
+@property (copy, nonatomic) NSString *title;
+@property (strong, nonatomic) PFUser *uploader;
+@property (copy, nonatomic) NSString *category;
+@property (copy, nonatomic) NSString *uploadDate;
+@property (copy, nonatomic) NSString *updatedDate;
+
+@property (strong, nonatomic) NSArray *photos;
+@property (strong, nonatomic) NSArray *comments;
+@property (strong, nonatomic) NSArray *likers;
+
 @end
